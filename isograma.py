3b41f8da6b6552c101feb13b7a442718cac04966
Nacho34234 <124839742+Nacho34234@users.noreply.github.com>
2023-02-16 02:46:47 +0000

    Leccion 4

---
+++ isograma.py
@@ -0,0 +1,19 @@
+palabra="papa"
+acumulate
+
+indice=0
+for letra in palabra:
+    print(indice,letra)
+    indice=indice+1
+
+print(indice)
+
+for letra in palabra:
+    if letra=="a":
+        acumulador=acumulador+1
+print(letra,acumulador)
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello")
+palabra="Nacho"
+
+for letra in palabra:
+    print(letra + "G"+"R")
+
+indice=0
+for letra in palabra:
+    print(indice,letra)
+    indice=indice+1
+
+marcas=["BMW","Honda","Nissan"]
+
+for marca in marcas:
+     print(marca)
+
+for indice,letra in enumerate(palabra):
+    print(indice,letra)
